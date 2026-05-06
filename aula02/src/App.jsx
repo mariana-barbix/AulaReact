933b2380abe672530521eb0885774d0740e7c649
mariana-barbix <marianabarbi39@gmail.com>
2026-05-06 14:43:50 -0300

    6/5

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -1,10 +1,14 @@
+import "./App.css";
+
 export default function App()
 {
   return(
     <div>
       <h1>Aula 02 - Estudo de Componentes e Props</h1>
 
-      
+      <div className="card"> 
+        <h3>Chamada para o Exemplo 1</h3>
+      </div>
     </div>
   )
 }
